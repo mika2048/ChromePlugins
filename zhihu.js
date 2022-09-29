f8dff6c8584d93c748c611422d47074377a0fb9c
mika2048 <cyno2038@gmail.com>
2022-09-29 11:41:48 +0800

    init

--- zhihu.js
+++ zhihu.js
@@ -1,7 +1,13 @@
 // ==UserScript==
 // @name         知乎增强
+// @name:zh-CN   知乎增强
+// @name:zh-TW   知乎增強
+// @name:en      Zhihu enhancement
 // @version      2.1.6
+// @author       X.I.U
 // @description  移除登录弹窗、屏蔽首页视频、默认收起回答、快捷收起回答/评论（左键两侧）、快捷回到顶部（右键两侧）、屏蔽用户、屏蔽关键词、移除高亮链接、屏蔽盐选内容、净化搜索热门、净化标题消息、展开问题描述、显示问题作者、置顶显示时间、完整问题时间、区分问题文章、直达问题按钮、默认高清原图、默认站外直链
+// @description:zh-TW  移除登錄彈窗、屏蔽首頁視頻、默認收起回答、快捷收起回答/評論、快捷回到頂部、屏蔽用戶、屏蔽關鍵詞、移除高亮鏈接、屏蔽鹽選內容、淨化搜索熱門、淨化標題消息、置頂顯示時間、完整問題時間、區分問題文章、默認高清原圖、默認站外直鏈...
+// @description:en  A more personalized Zhihu experience~
 // @match        *://www.zhihu.com/*
 // @match        *://zhuanlan.zhihu.com/*
 // @exclude      https://www.zhihu.com/signin*
@@ -15,7 +21,11 @@
 // @grant        GM_notification
 // @grant        GM_info
 // @grant        window.onurlchange
+// @license      GPL-3.0 License
 // @run-at       document-end
+// @namespace    https://greasyfork.org/scripts/4122051
+// @supportURL   https://github.com/XIU2/UserScript
+// @homepageURL  https://github.com/XIU2/UserScript
 // ==/UserScript==
  
 'use strict';
